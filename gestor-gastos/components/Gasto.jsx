f269086d14c59d8fd569f017a4ee37c51f78aefb
juan.abad@bootcamp-upgrade.com <iron-man@avengers.com>
2023-08-25 13:05:26 +0200

    Iconos, revisar errores

--- gestor-gastos/components/Gasto.jsx
+++ gestor-gastos/components/Gasto.jsx
@@ -1,15 +1,43 @@
 import React from "react";
+import { formatearFecha } from "../src/helpers";
+import IconoAhorro from '../src/img/icono_ahorro.svg'
+import IconoCasa from '../src/img/icono_casa.svg'
+import IconoComida from '../src/img/icono_comida.svg'
+import IconoGastos from '../src/img/icono_gastos.svg'
+import IconoOcio from '../src/img/icono_ocio.svg'
+import IconoOcio from '../src/img/icono_salud.svg'
+import IconoSuscripciones from '../src/img/icono_suscripciones.svg'
+
+const diccionarioIconos = {
+  ahorro : IconoAhorro,
+  comida : IconoComida,
+  casa : IconoCasa,
+  gastos : IconoGastos,
+  ocio : IconoOcio,
+  salud : IconoOcio,
+  suscripciones :IconoSuscripciones
+}
+
 
 const Gasto = ({ gasto }) => {
-  const { categoria, nombre, cantidad, id } = gasto;
+  const { categoria, nombre, cantidad, id, fecha } = gasto;
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
+      <img
+      src={diccionarioIconos[categoria]}
+      alt="Icono Gasto"
+      />
         <div className="descripcion-gasto">
           <p className="categoria">{categoria}</p>
           <p className="nombre-gasto">{nombre}</p>
+          <p className= "fecha-gasto">
+            Agregado el: {''}
+            <span>{formatearFecha(fecha)}</span>
+          </p>
         </div>
       </div>
+      <p className="cantidad-gasto">{cantidad}€</p>
     </div>
   );
 };
